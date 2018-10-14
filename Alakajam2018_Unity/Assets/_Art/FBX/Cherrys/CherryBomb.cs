2c3193009670c82840ca5bd55b41b1a7ae0d21c9
Rasmus Thorup <rasmusbthorup@gmail.com>
2018-10-14 21:00:27 +0200

    Cherry

---
+++ Alakajam2018_Unity/Assets/_Art/FBX/Cherrys/CherryBomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryBomb : MonoBehaviour {
+
+    public float fallingSpeed = 15f;
+
+    public PlayerController playerController;
+    public Rigidbody playerRigid;
+
+    // Use this for initialization
+    void Start () {
+        GameObject player = GameObject.FindWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+        playerRigid = player.GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.down * fallingSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameManager.instance.gameOver)
+        {
+            return;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            playerController.enabled = false;
+
+            //Stopping Player momentum
+            playerRigid.velocity = Vector3.zero;
+            playerRigid.useGravity = true;
+
+        }
+        if (other.CompareTag("Despawner"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Alakajam2018_Unity/Assets/_Art/FBX/Cherrys/CherrySpawner.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherrySpawner : MonoBehaviour {
+
+    public GameObject[] cherryBombsToSpawn;
+
+    public Transform playerTrans;
+
+    public float yOffset = 16;
+
+    [Header("Randomly Spawns Between")]
+    public float spawnIntervalMin = 1;
+    public float spawnIntervalMax = 5;
+
+    public GameObject warningGo;
+
+    public float warningTime = 1.5f;
+
+    private float time;
+
+
+    public bool startBombing;
+
+    public static bool bombingActive;
+
+    void Start () {
+
+        foreach (var cherry in cherryBombsToSpawn)
+        {
+
+            cherry.SetActive(false);
+        }
+
+        GameObject player = GameObject.FindWithTag("Player");
+        playerTrans= player.transform;
+
+        warningGo.SetActive(false);
+
+    }
+	
+
+	void Update () {
+
+        if (GameManager.instance.gameOver)
+        {
+            return;
+        }
+
+        if (!startBombing)
+        {
+            return;
+        }
+        
+        if (bombingActive)
+        {
+            transform.position = new Vector3(transform.position.x, playerTrans.position.y + yOffset, transform.position.z);
+            return;
+        }
+
+        transform.position = new Vector3(playerTrans.position.x, playerTrans.position.y + yOffset, playerTrans.position.z);
+
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            StartCoroutine(SpawnCherryBomb());
+            time = Random.Range(spawnIntervalMin, spawnIntervalMax);
+        }
+    }
+
+    IEnumerator SpawnCherryBomb(){
+
+        warningGo.SetActive(true);
+        bombingActive = true;
+
+        yield return new WaitForSeconds(warningTime);
+
+        warningGo.SetActive(false);
+
+        GameObject cherryGO = cherryBombsToSpawn[Random.Range(0, 1)];
+        cherryGO.SetActive(true);
+        cherryGO.transform.position = transform.position;
+
+
+        yield return null;
+    }
+}
