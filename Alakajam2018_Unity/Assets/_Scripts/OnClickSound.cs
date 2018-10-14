dab314a81330674b81afbb31f1b3d757b5f5e90c
Jeppe Lindskov <jeppelind@hotmail.com>
2018-10-14 13:13:01 +0200

    Menu Sound In

---
+++ Alakajam2018_Unity/Assets/_Scripts/OnClickSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FMODUnity;
+
+
+
+public class OnClickSound : MonoBehaviour {
+
+
+    public StudioEventEmitter onClick; 
+
+
+    public void PlaySoundOnClick()
+    {
+        onClick.Play(); 
+    }
+
+
+}
